b6cf2b678081e591ea92c482c877028ae79f25e5
장혜승 <haileyjang@bizinsight.co.kr>
2023-11-01 11:23:59 +0900

    browser-image-compression test

--- app/browserimagecompression/page.tsx
+++ app/browserimagecompression/page.tsx
@@ -1,7 +1,82 @@
-export default async function BrowserImageCompression() {
+"use client";
+
+import { atom, useRecoilState } from "recoil";
+import Resizer from "react-image-file-resizer";
+import { ChangeEvent } from "react";
+import imageCompression from "browser-image-compression";
+
+type ImageFile = File & {
+  readonly type: "image/jpeg" | "image/png";
+};
+
+const fileState = atom<File | string | null>({
+  key: "fileState",
+  default: null,
+});
+
+const previewState = atom<string | null>({
+  key: "previewState",
+  default: null,
+});
+
+const fileUrlState = atom<string>({
+  key: "fileUrlState",
+  default: "",
+});
+
+export default function ReactImageFileResizer() {
+  const [, setFile] = useRecoilState(fileState);
+  const [fileUrl, setFileUrl] = useRecoilState(fileUrlState);
+
+  const handleFileOnChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      let file = (await e.target.files[0]) as ImageFile;
+
+      const options = {
+        maxSizeMB: 60,
+        maxWidthOrHeight: 1920,
+        useWebWorker: true,
+        initialQuality: 0.5,
+      };
+
+      try {
+        const compressedFile = await imageCompression(file, options);
+        console.log(
+          "compressedFile instanceof Blob",
+          compressedFile instanceof Blob
+        ); // true
+        console.log(
+          `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
+        ); // smaller than maxSizeMB
+
+        setFile(compressedFile);
+
+        // resize된 이미지의 url을 받아 fileUrl에 저장
+        const promise = imageCompression.getDataUrlFromFile(compressedFile);
+        promise.then((result) => {
+          setFileUrl(result);
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>BrowserImageCompression</h1>
+      <h1>Browser Image Compression</h1>
+      <input
+        type="file"
+        accept="image/jpg,image/png,image/jpeg"
+        id="profile_img_upload"
+        onChange={handleFileOnChange}
+      />
+      {/* 
+        <label for="profile_img_upload">
+          <img src={camera} alt="camera" />
+        </label> */}
+
+      <img className="top_bar_profile_img" src={fileUrl} alt="profile_img" />
     </div>
   );
 }
--- app/reactimagefileresizer/page.tsx
+++ app/reactimagefileresizer/page.tsx
@@ -4,7 +4,11 @@ import { atom, useRecoilState } from "recoil";
 import Resizer from "react-image-file-resizer";
 import { ChangeEvent } from "react";
 
-const fileState = atom<File | null>({
+type ImageFile = File & {
+  readonly type: "image/jpeg" | "image/png";
+};
+
+const fileState = atom<File | string | null>({
   key: "fileState",
   default: null,
 });
@@ -18,19 +22,52 @@ export default function ReactImageFileResizer() {
   const [, setSelectedFile] = useRecoilState(fileState);
   const [previewUrl, setPreviewUrl] = useRecoilState(previewState);
 
-  const onFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onResizingImage = (file: File) => {
+    let resizedUri;
+    return new Promise((resolve) => {
+      Resizer.imageFileResizer(
+        file,
+        500,
+        500,
+        "JPEG",
+        100,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        "file" // outputType : base64, blob or file, default type is base64
+      );
+    });
+  };
+
+  const onFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
       // 이미지 리사이징
+      const file = (await e.target.files[0]) as ImageFile;
 
-      setSelectedFile(e.target.files[0]);
+      // 파일 형식이 지원되는지 확인 (JPEG, JPG, PNG)
+      const supportedFormats = ["image/jpeg", "image/png"];
+      if (!supportedFormats.includes(file.type)) {
+        alert(
+          "지원되지 않는 이미지 형식입니다. JPEG, JPG, PNG 형식의 이미지를 업로드해주세요."
+        );
+        return;
+      }
+
+      const resizedImage = (await onResizingImage(file)) as ImageFile;
+      //console.log(typeof resizedImage); // object
+      console.log(resizedImage);
+
+      setSelectedFile(resizedImage);
 
       // FileReader 객체 생성
       const reader = new FileReader();
       reader.onloadend = () => {
         // 파일 읽기가 완료되면, 미리보기 URL을 상태에 저장
-        setPreviewUrl(reader.result as string);
+        setPreviewUrl(reader.result as string); // 원본인 것 같다.
       };
-      reader.readAsDataURL(e.target.files[0]);
+      //reader.readAsDataURL(file);             // 원본
+      reader.readAsDataURL(resizedImage); // 리사이즈된 이미지
     }
   };
 
@@ -45,7 +82,8 @@ export default function ReactImageFileResizer() {
         <input type="file" onChange={onFileChange} />
         <button onClick={onFileUpload}>파일 업로드</button>
       </div>
-      {previewUrl && <img src={previewUrl} alt="preview" width="100" />}
+      {/* <button onClick={onResizingImage}></button> */}
+      {previewUrl && <img src={previewUrl} alt="preview" />}
     </div>
   );
 }
