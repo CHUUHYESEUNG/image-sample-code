a035d38fd35346ddd7501445f6f7ac09455daa9e
장혜승 <haileyjang@bizinsight.co.kr>
2023-11-01 13:36:29 +0900

    heic convert library 추가

--- app/heicconvert/page.tsx
+++ app/heicconvert/page.tsx
@@ -1,7 +1,33 @@
-export default async function HeicConvert() {
+"use client";
+
+import React, { ChangeEvent } from "react";
+import heic2any from "heic2any";
+
+export default function HeicConvert() {
+  const convertHeicToJpg = async (file: File) => {
+    const result = await heic2any({
+      blob: file,
+      toType: "image/jpeg",
+      quality: 0.5,
+    });
+
+    console.log(result);
+
+    const url = URL.createObjectURL(result);
+    //console.log(url);
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      convertHeicToJpg(file);
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>HeicConvert</h1>
+      <input type="file" onChange={handleFileChange} />
     </div>
   );
 }
