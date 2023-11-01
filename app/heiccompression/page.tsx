dabb81cadc7d49dc68e9d25d617d1902e41aa7a2
장혜승 <haileyjang@bizinsight.co.kr>
2023-11-01 18:05:28 +0900

    확장자 Sync 맞춰야하는 문제 있음

--- app/heiccompression/page.tsx
+++ app/heiccompression/page.tsx
@@ -5,8 +5,10 @@ import React, { ChangeEvent } from "react";
 import heic2any from "heic2any";
 import imageCompression from "browser-image-compression";
 
+const ALLOW_FILE_EXTENSION = "jpg, jpeg, png, heic"; // 허용 가능한 확장자
+
 type ImageFile = File & {
-  readonly type: "image/jpeg" | "image/png";
+  readonly type: "image/jpeg" | "image/png" | "image/heic";
 };
 
 const fileState = atom<File | string | null>({
@@ -20,7 +22,7 @@ const fileUrlState = atom<string>({
 });
 
 export default function HeicCompression() {
-  const [, setFile] = useRecoilState(fileState);
+  const [file, setFile] = useRecoilState(fileState);
   const [fileUrl, setFileUrl] = useRecoilState(fileUrlState);
 
   const convertHeicToJpg = async (file: File) => {
@@ -30,6 +32,8 @@ export default function HeicCompression() {
       quality: 0.5,
     });
 
+    //setFile(result);
+
     console.log(result);
 
     const fileUrl = URL.createObjectURL(result); // blob파일을 URL로 변환
@@ -37,19 +41,41 @@ export default function HeicCompression() {
     setFileUrl(fileUrl);
   };
 
-  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      convertHeicToJpg(file);
+  const removeFileName = (originalFileName: string): string => {
+    // 순수 파일 확장자 return
+    const lastIndex = originalFileName.lastIndexOf(".");
+
+    if (lastIndex < 0) {
+      return "";
     }
+
+    return originalFileName.substring(lastIndex + 1).toLowerCase();
   };
 
   const handleFileOnChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    // 들어온 파일의 확장자가 heic이냐 아니냐로 구분
+    // 만약 heic, jpg, png, jpeg가 아니라면 제대로 된 파일 달라고 유효성 검사 해야함.
+    // heic이냐 아니냐에 따라 분기 처리
+    // 사진 최대 용량 2MB 언더
+
+    if (e.target.files === undefined) return;
+
     if (e.target.files && e.target.files.length > 0) {
       let file = (await e.target.files[0]) as ImageFile;
+      let fileName = await e.target.files[0].name;
+
+      let fileExtension = removeFileName(fileName).toLowerCase();
+      if (fileExtension === "heic") {
+        // Heic to jpg
+        //let file = e.target.files?.[0];
+        //if (file) {
+        //setFile(convertHeicToJpg(file));
+        // convertHeicToJpg(file) 반환값이 promise<void> 객체여서 setFile 인자 타입과 Sync 맞춰야 한다.
+        //}
+      }
 
       const options = {
-        maxSizeMB: 60,
+        maxSizeMB: 2,
         maxWidthOrHeight: 1920,
         useWebWorker: true,
         initialQuality: 0.5,
@@ -60,10 +86,10 @@ export default function HeicCompression() {
         console.log(
           "compressedFile instanceof Blob",
           compressedFile instanceof Blob
-        ); // true
+        );
         console.log(
           `compressedFile size ${compressedFile.size / 1024 / 1024} MB`
-        ); // smaller than maxSizeMB
+        );
 
         setFile(compressedFile);
 
@@ -80,24 +106,16 @@ export default function HeicCompression() {
 
   return (
     <>
-      <h1>1. Heic Convert </h1>
-
       <div className="flex flex-col items-center justify-between p-24">
-        <input type="file" onChange={handleFileChange} />
-      </div>
-      <div>{fileUrl && <img src={fileUrl} alt="Example" />}</div>
-
-      <h1>2. Image Compression </h1>
-      <div className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Heic Convert to Image Compression</h1>
         <input
           type="file"
-          accept="image/jpg,image/png,image/jpeg"
+          accept=".jpg,.png,.jpeg,.heic"
           id="profile_img_upload"
           onChange={handleFileOnChange}
         />
-
-        <img className="top_bar_profile_img" src={fileUrl} alt="profile_img" />
       </div>
+      <div>{fileUrl && <img src={fileUrl} />}</div>
     </>
   );
 }
