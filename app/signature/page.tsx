c95704d5d6bf4e5ab56e90e4b6e0674380c494e2
장혜승 <haileyjang@bizinsight.co.kr>
2023-11-01 14:50:15 +0900

    signature pad 구현

--- app/signature/page.tsx
+++ app/signature/page.tsx
@@ -1,7 +1,60 @@
-export default async function Signature() {
+"use client";
+
+import React, { useRef } from "react";
+import { atom, useRecoilState } from "recoil";
+import ReactSignatureCanvas from "react-signature-canvas";
+import "./style.css";
+
+const isSignState = atom<boolean>({
+  key: "isSignState",
+  default: false,
+});
+
+const isSignUrlState = atom<string | null>({
+  key: "isSignUrlState",
+  default: null,
+});
+
+export default function Signature() {
+  // const signCanvas = useRef() as React.MutableRefObject<any>;
+  const signCanvas = useRef<any>(null);
+  const [isSigned, setIsSigned] = useRecoilState(isSignState);
+  const [isSignUrl, setIsSignUrl] = useRecoilState(isSignUrlState);
+
+  const save = () => {
+    const savedSignImageUrl = signCanvas.current
+      .getTrimmedCanvas()
+      .toDataURL("image/png");
+    setIsSignUrl(savedSignImageUrl);
+    const link = document.createElement("a");
+    link.href = savedSignImageUrl;
+    link.download = "sign_image.png";
+    link.click();
+  };
+
+  const clear = () => {
+    signCanvas.current.clear();
+    setIsSigned(false);
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Signature</h1>
+      <h1>Signature Canvas Test</h1>
+      <div className="canvasContainer">
+        <ReactSignatureCanvas
+          ref={signCanvas}
+          canvasProps={{ className: "sigCanvas canvasStyle" }}
+          backgroundColor="rgb(230, 230, 230)"
+          onBegin={() => {
+            setIsSigned(true);
+          }}
+        />
+      </div>
+      <button onClick={() => clear()}>clear</button>
+      <button disabled={!isSigned} onClick={() => save()}>
+        save
+      </button>
+      {isSignUrl ? <img className={"signImage"} src={isSignUrl} /> : null}
     </div>
   );
 }
