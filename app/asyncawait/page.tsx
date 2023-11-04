1d92ed713e9fc74c05afe12ab5999b48350213fa
chuuhyeseung <cheesehsmay5th@gmail.com>
2023-11-04 22:10:54 +0900

    Promise 예시 추가

--- app/asyncawait/page.tsx
+++ app/asyncawait/page.tsx
@@ -1,5 +1,23 @@
 "use client";
 
+const timer = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1);
+  }, 1000);
+});
+
 export default function Asyncawait() {
+  // Promise
+  timer
+    .then(function () {
+      console.log("작업1");
+    })
+    .then(function () {
+      console.log("작업2");
+    })
+    .then(function () {
+      console.log("작업3");
+    });
+
   return <div>asyncawait</div>;
 }
